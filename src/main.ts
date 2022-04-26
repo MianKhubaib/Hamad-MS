8e9dd41e552af61cfef0dfee6fdad5b5339900db
Mian Khubaib <miankhubaib63@gmail.com>
2022-04-26 13:08:13 +0500

    port issue

--- src/main.ts
+++ src/main.ts
@@ -32,8 +32,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/apidocs', app, document);
 
-  const port = process.env.DB_PORT;
-  await app.listen(port);
+  const port = process.env.DB_PORT || 3001;
+  await app.listen(port, process.env.DB_HOST, function () {
+    console.log('listening on port and server', port);
+  });
   console.log('App running at', port);
 }
 bootstrap();
