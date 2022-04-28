782166b0fa679c5be46ff2387fb076095bfdb3b3
Mian Khubaib <miankhubaib63@gmail.com>
2022-04-29 00:42:14 +0500

    api updated, Rowkey issues still there

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,9 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  forwardRef,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import {
   AzureTableStorageResponse,
   AzureTableStorageResultList,
@@ -21,8 +26,7 @@ export class UserService {
       throw new Error('Invalid user');
     }
     user['PartitionKey'] = user.persona;
-    //user.userId =
-    return await this.userRepository.create(user, '1');
+    return await this.userRepository.create(user, uuid());
   }
   async getAll() {
     return await this.userRepository.findAll();
@@ -31,13 +35,27 @@ export class UserService {
   async getByRK(RK: string, user: User) {
     const query = new TableQuery();
     //  return await this.userRepository.find(RK, user);
-    const email = 'khubaib1@gmail.com';
+    return await this.userRepository.findAll(query.where(`RowKey == '${RK}'`));
+    // if (users['data']['entries'].length > 0) {
+    //   return users['data']['entries'][0];
+    // }
+    // return new BadRequestException('Entity does not exist');
+  }
+  async getByemail(email: string, user: User) {
+    const query = new TableQuery();
+    //  return await this.userRepository.find(RK, user);
     return await this.userRepository.findAll(
       query.where(`email == '${email}'`),
     );
   }
-
-  async deleteByRK(RK: string, user: User): Promise<AzureTableStorageResponse> {
+  // we need rowkey to delete but it say asset not found
+  async deleteByRK(RK: string, user: User) {
     return await this.userRepository.delete(RK, user);
+    // const query = new TableQuery();
+    // const user1 = await this.userRepository.findAll(
+    //   query.where(`RowKey == '${RK}'`),
+    // );
+    // console.log(user1['data'['entries']]);
+    // return await user1['data'];
   }
 }
