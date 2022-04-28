54ff9e38c7c550fd6950ab54e233765463718759
Mian Khubaib <miankhubaib63@gmail.com>
2022-04-28 17:25:39 +0500

    api added for request static

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,6 +15,7 @@ import { ApiOperation } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { UserDTO } from './dto/user.dto';
 import { UpdatePersonaDTO } from './dto/updatePersona.dto';
+
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -31,6 +32,78 @@ export class UserController {
     } catch (e) {
       throw new BadRequestException(e.message);
     }
+  }
+
+  @Post('/request')
+  async request() {
+    return 'Request Submitted';
+  }
+
+  @Get('getrequest')
+  async allRequest() {
+    return [
+      {
+        id: '1',
+        Title: 'Covid 19 Reports',
+        RequestedOn: '1-04-2022',
+        purpose: 'Needed for audit',
+        expectedBy: '4-04-2022',
+        Status: 'Pending',
+        currentApprover: 'Kazim',
+      },
+      {
+        id: '2',
+        Title: 'Employee Reports',
+        RequestedOn: '2-04-2022',
+        purpose: 'Needed for appraisel',
+        expectedBy: '5-04-2022',
+        Status: 'Pending',
+        currentApprover: 'Ahmed Hasan',
+      },
+    ];
+  }
+
+  @Get('/requestDetail')
+  requestDetail() {
+    return {
+      id: '2',
+      Title: 'Employee Reports',
+      SubmittedOn: '2-04-2022',
+      Purpose: 'Needed for appraisel',
+      ExpectedBy: '5-04-2022',
+      Status: 'Pending',
+      CurrentApprover: 'Ahmed Hasan',
+      Attachments: ['Mock Up.ppt', 'Report.word'],
+      SubmittedBy: 'Mian Khubaib',
+      ResearchProject: 'Yes',
+      Approvers: [
+        {
+          name: 'Bilal',
+          approvedStatus: 'No',
+          Date: '',
+        },
+        {
+          name: 'Sarah',
+          approvedStatus: 'Yes',
+          Date: '22-01-2022',
+        },
+      ],
+      Comments: [
+        {
+          name: 'Kazim',
+          text: 'TA has been assigned',
+        },
+        {
+          name: 'Mian Khubaib',
+          text: 'BA has been assigned',
+        },
+      ],
+    };
+  }
+
+  @Delete('/request/withdraw')
+  requestWithdraw() {
+    return 'Request Withdrawed Successfully';
   }
 
   @Get('/all')
